ce2bf63ae013a46898b4d8ab26ac4a73f4f1ce61
YutongHu-Natalie <yutong.hu@emory.edu>
2026-01-15 09:11:50 -0500

    update results for llama 2 7b

--- LLM_code/train_and_inference.sh
+++ LLM_code/train_and_inference.sh
@@ -4,17 +4,17 @@ FLAG=1
 # Please adjust the following parameters according to your needs. Rememeber to update the MODELPATH for each LLM model.
 
 # ------  select basemodel ----------
-# MODEL_NAME='LLaMA2'
+MODEL_NAME='LLaMA2'
 # MODEL_NAME='LLaMA3'
-MODEL_NAME='LLaMA3-instruct-70b'
+# MODEL_NAME='LLaMA3-instruct-70b'
 # MODEL_NAME='LLaMA3-instruct'
 # MODEL_NAME='Phi3-medium'
 
 # ------ select the experiment ------------
 # Experiments_setting='test'
 # Experiments_setting='zero_shot'
-# Experiments_setting='few_shot'
-Experiments_setting='lora'
+Experiments_setting='few_shot'
+# Experiments_setting='lora'
 # Experiments_setting='all_parameters'
 
 #  ------ select the dataset ------ 
